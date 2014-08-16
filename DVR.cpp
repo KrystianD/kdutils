9f617036d484e79f7fa03b98ccdcb3f23b59414a
Krystian Dużyński <krystekd@gmail.com>
2014-08-16 16:14:17 +0200

    DVR: conditional logging

--- DVR.cpp
+++ DVR.cpp
@@ -17,20 +17,25 @@
 #include <errno.h>
 #include <string.h>
 
+#ifdef DVR_LOG
 #define LOG(x,...) fprintf(stderr, "[DVR] " x "\n", ##__VA_ARGS__)
 #define LOGCH(ch, x,...) fprintf(stderr, "[DVR #%d] " x "\n", ch, ##__VA_ARGS__)
+#else
+#define LOG(x,...)
+#define LOGCH(x,...)
+#endif
 
 class TDVRPacket
 {
 public:
 	unsigned char raw[32];
-	
+
 	TDVRPacket()
 	{
 		memset(raw, 0, sizeof(raw));
 		//raw[3] = 0x28; // size_version
 	}
-	
+
 	void setCmd(uint8_t cmd)
 	{
 		raw[0] = cmd;
@@ -76,14 +81,14 @@ bool DVR::connectControl(const string& host, uint16_t port)
 {
 	m_host = host;
 	m_port = port;
-	
+
 	struct sockaddr_in serv_addr;
 	m_controlFd = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
-	
+
 	hostent* ent = gethostbyname(m_host.c_str());
 	if (!ent)
 	{
@@ -91,13 +96,13 @@ bool DVR::connectControl(const string& host, uint16_t port)
 		return false;
 	}
 	memcpy(&serv_addr.sin_addr, ent->h_addr_list[0], 4);
-	
+
 	if (connect(m_controlFd, (sockaddr*)&serv_addr, sizeof(serv_addr)))
 	{
 		setError(ConnectError, getErrorText("connect error"));
 		return false;
 	}
-	
+
 	return true;
 }
 void DVR::disconnect()
@@ -122,13 +127,13 @@ bool DVR::login(const string& username, const string& pass)
 {
 	TDVRPacket pk;
 	int res;
-	
+
 	char user_pass[20];
 	sprintf(user_pass, "%s&&%s", username.c_str(), pass.c_str());
-	
+
 	strncpy((char*)&pk.raw[8], username.c_str(), 8);
 	strncpy((char*)&pk.raw[16], pass.c_str(), 8);
-	
+
 	pk.setCmd(0xa0);
 	//pk.raw[3] = 0x60;
 	pk.raw[24] = 0x04;
@@ -136,26 +141,26 @@ bool DVR::login(const string& username, const string& pass)
 	//pk.raw[30] = 0xa1;
 	//pk.raw[31] = 0xaa;
 	//pk.setExtLen (strlen (user_pass));
-	
+
 	res = senddata(m_controlFd, pk.raw, 32);
 	if (res == -1)
 	{
 		setError(ConnectionError, getErrorText("unable to send login request packet"));
 		return false;
-	}	
+	}
 	res = readdata(m_controlFd, pk.raw, 32);
 	if (res == -1)
 	{
 		setError(ConnectionError, getErrorText("unable to read login response packet"));
 		return false;
 	}
-	
+
 	if (pk.getCmd() != 0xb0)
 	{
 		setError(ConnectionError, "invalid login response");
 		return false;
 	}
-	
+
 	res = pk.raw[8];
 	if (res != 0)
 	{
@@ -165,7 +170,7 @@ bool DVR::login(const string& username, const string& pass)
 		setError(LoginError, buf);
 		return false;
 	}
-	
+
 	m_connId = *(uint32_t*)(pk.raw + 16);
 
 	return true;
@@ -174,7 +179,7 @@ bool DVR::createChannel(int num, int frameCapacity)
 {
 	TDVRPacket pk;
 	int res;
-	
+
 	TStreamConnection strm;
 	strm.channel = num;
 	strm.buffer = 0;
@@ -185,29 +190,29 @@ bool DVR::createChannel(int num, int frameCapacity)
 	strm.frameRead = 0;
 	strm.frameReady = 0;
 	strm.buffer = new char[frameCapacity];
-	
+
 	struct sockaddr_in serv_addr;
 	strm.fd = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(m_port);
-	
+
 	hostent* ent = gethostbyname(m_host.c_str());
 	memcpy(&serv_addr.sin_addr, ent->h_addr_list[0], 4);
-	
+
 	if (connect(strm.fd, (sockaddr*)&serv_addr, sizeof(serv_addr)))
 	{
 		setError(ConnectError, "Unable to create stream connection");
 		return false;
 	}
-	
+
 	pk.setCmd(0xf1);
 	pk.setU32(8, m_connId);
 	pk.raw[12] = 1;
 	pk.raw[13] = num + 1;
 	pk.raw[17] = 0; // 0-estabilish, 1-close
-	
+
 	res = senddata(strm.fd, pk.raw, 32);
 	if (res == -1)
 	{
@@ -220,7 +225,7 @@ bool DVR::createChannel(int num, int frameCapacity)
 		setError(ConnectionError, getErrorText("unable to read conn bind response packet"));
 		return false;
 	}
-	
+
 	res = pk.raw[14];
 	if (res != 0)
 	{
@@ -231,9 +236,9 @@ bool DVR::createChannel(int num, int frameCapacity)
 		return false;
 	}
 	LOG("created for fd %d", strm.fd);
-	
+
 	m_conns.push_back(strm);
-	
+
 	return true;
 }
 bool DVR::removeChannel(int channel)
@@ -241,18 +246,18 @@ bool DVR::removeChannel(int channel)
 	TDVRPacket pk;
 	int res;
 	TStreamConnection *strm = 0;
-	
+
 	for (size_t i = 0; !strm && i < m_conns.size(); i++)
 		if (m_conns[i].channel == channel)
 			strm = &m_conns[i];
 	if (!strm) return false;
-	
+
 	pk.setCmd(0xf1);
 	pk.setU32(8, m_connId);
 	pk.raw[12] = 1;
 	pk.raw[13] = channel + 1;
 	pk.raw[17] = 1; // 0-estabilish, 1-close
-	
+
 	res = senddata(strm->fd, pk.raw, 32);
 	if (res == -1)
 	{
@@ -265,7 +270,7 @@ bool DVR::removeChannel(int channel)
 		setError(ConnectionError, getErrorText("unable to read conn unbind response packet"));
 		return false;
 	}
-	
+
 	res = pk.raw[14];
 	if (res != 0)
 	{
@@ -275,27 +280,27 @@ bool DVR::removeChannel(int channel)
 		setError(LoginError, buf);
 		return false;
 	}
-	
+
 	close(strm->fd);
-	
+
 	delete [] strm->buffer;
 	for (size_t i = 0; i < m_conns.size(); i++)
 		if (m_conns[i].channel == channel)
 			m_conns.erase(m_conns.begin() + i);
-			
+
 	return true;
 }
 bool DVR::updateChannels()
 {
 	TDVRPacket pk;
 	int res;
-	
+
 	pk.setCmd(0x11);
 	pk.setExtLen(16);
-	
+
 	for (size_t i = 0; i < m_conns.size(); i++)
 		pk.raw[8 + m_conns[i].channel] = 1; // 1-yes, 0-no
-		
+
 	res = senddata(m_controlFd, pk.raw, 32);
 	if (res == -1)
 	{
@@ -304,11 +309,11 @@ bool DVR::updateChannels()
 	}
 	uint8_t extdata[16];
 	memset(extdata, 0, 16);
-	
+
 	// if 1, only thumbnails are being sent
 	for (size_t i = 0; i < m_conns.size(); i++)
 		extdata[m_conns[i].channel] = 0;
-		
+
 	res = senddata(m_controlFd, extdata, 16);
 	if (res == -1)
 	{
@@ -321,9 +326,9 @@ bool DVR::ping()
 {
 	TDVRPacket pk;
 	int res;
-	
+
 	pk.setCmd(0xa1);
-	
+
 	res = senddata(m_controlFd, pk.raw, 32);
 	if (res == -1)
 	{
@@ -344,7 +349,7 @@ bool DVR::checkForData()
 {
 	fd_set fds;
 	FD_ZERO(&fds);
-	
+
 	int maxFd = -1;
 	for (size_t i = 0; i < m_conns.size(); i++)
 	{
@@ -352,7 +357,7 @@ bool DVR::checkForData()
 		FD_SET(strm->fd, &fds);
 		if (strm->fd > maxFd) maxFd = strm->fd;
 	}
-	
+
 	timeval tv;
 	tv.tv_sec = 0;
 	tv.tv_usec = 200 * 1000;
@@ -362,13 +367,13 @@ bool DVR::checkForData()
 		for (size_t i = 0; i < m_conns.size(); i++)
 		{
 			TStreamConnection *strm = &m_conns[i];
-			
+
 			if (strm->frameReady)
 				continue;
 
 			if (!FD_ISSET(strm->fd, &fds))
 				continue;
-				
+
 			if (strm->packetLen == 0) // we're waiting for header
 			{
 				TDVRPacket pk;
@@ -378,7 +383,7 @@ bool DVR::checkForData()
 					setError(ConnectionError, getErrorText("unable to read frame header"));
 					return false;
 				}
-				
+
 				if (pk.getCmd() != 0xbc)
 				{
 					char buf[200];
@@ -386,7 +391,7 @@ bool DVR::checkForData()
 					setError(UnknownError, buf);
 					return false;
 				}
-				
+
 				res = pk.raw[16];
 				if (res != 0)
 				{
@@ -395,7 +400,7 @@ bool DVR::checkForData()
 					setError(ConnectionError, buf);
 					return false;
 				}
-				
+
 				strm->packetLen = pk.getExtLen();
 				strm->packetRead = 0;
 			}
@@ -455,12 +460,12 @@ bool DVR::checkForData()
 bool DVR::isAvail(int channel)
 {
 	TStreamConnection *strm = 0;
-	
+
 	for (size_t i = 0; !strm && i < m_conns.size(); i++)
 		if (m_conns[i].channel == channel)
 			strm = &m_conns[i];
 	if (!strm) return false;
-	
+
 	return strm->frameReady;
 }
 bool DVR::grabFrame(int channel, char** buffer, int* len)
@@ -519,7 +524,7 @@ int DVR::readdata(int fd, uint8_t* data, int len)
 {
 	//int flags = fcntl (fd, F_GETFL, 0);
 	///fcntl (fd, F_SETFL, flags | O_NONBLOCK);
-	
+
 	int totalRead = 0;
 	while (totalRead < len)
 	{
